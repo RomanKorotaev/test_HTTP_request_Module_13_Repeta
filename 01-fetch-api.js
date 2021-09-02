c97ac3bd8b0584134a331fc78ede7fca122b5cc4
Roman Korotaev <kro1986@ukr.net>
2021-09-02 22:36:14 +0300

    Сделан Altrt при ошибке в catch

--- 01-fetch-api.js
+++ 01-fetch-api.js
@@ -1,31 +1,51 @@
 import pokemonCardTpl from './templates/pokemon-card.hbs'
 
-
-
-const refs = {
-    cardContainer: document.querySelector('.js-card-container')
-}
-
-
-
 console.log('Hello, World! (01-fetch-api.js)')
 
 
-fetchPokemonById()
+const refs = {
+    cardContainer: document.querySelector('.js-card-container'),
+    searchForm: document.querySelector('.js-search-form')
+}
 
-function fetchPokemonById() {
-     fetch('https://pokeapi.co/api/v2/pokemon/10')
-    .then(response => {
-        return response.json();
-    })
+refs.searchForm.addEventListener('submit', onSearch);
+
+// fetchPokemon(5)
+//     .then(renderPocemonCard)
+//     .catch(error => console.log(error));
+
+function onSearch(e) {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    //Это пример селектора в js по свойству name (если оно задано єлементу в html-файле)
+    //Репета: Модуль 12 НТТР-запросы; время:  1:13:50
+    const searchQuery = form.elements.query.value;
+    
+    fetchPokemon(searchQuery)
     .then(renderPocemonCard)
-    .catch(error => {
-        console.log(error);
-    } )
+        .catch(error => {
+            //console.log('Это CATCH');
+           // console.dir(error);
+            onFetchError (error)
+        })
+    .finally ( () => form.reset()) // для офистки формы
+ }
+
+
+function fetchPokemon(pokemonId) {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+        .then(response => response.json());
+   
  };
 
 function renderPocemonCard(pokemon) {
      const markup = pokemonCardTpl(pokemon);
     //console.log(markup);
     refs.cardContainer.innerHTML = markup;
+ }
+
+
+function onFetchError(error) {
+    alert ("Упс, что-то пошло не так и мы не нашли Вашего покемона.")
  }
