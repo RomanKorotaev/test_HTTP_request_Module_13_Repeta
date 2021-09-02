18015029f67bfc373a07d82c935abc4c882b3f64
Roman Korotaev <kro1986@ukr.net>
2021-09-02 18:58:18 +0300

    Отрендерили одну пробную карточку

--- 01-fetch-api.js
+++ 01-fetch-api.js
@@ -2,12 +2,23 @@ import pokemonCardTpl from './templates/pokemon-card.hbs'
 
 
 
-const r = fetch('https://pokeapi.co/api/v2/pokemon')
+const refs = {
+    cardContainer: document.querySelector('.js-card-container')
+}
+
+
+
+console.log('Hello, World! (01-fetch-api.js)')
+
+const r = fetch('https://pokeapi.co/api/v2/pokemon/1')
     .then(response => {
         return response.json();
     })
     .then(pokemon => {
         console.log(pokemon);
+        const markup = pokemonCardTpl(pokemon);
+        //console.log(markup);
+        refs.cardContainer.innerHTML = markup;
     })
     .catch(error => {
         console.log(error);
